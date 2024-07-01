38bb97743bfb67ab7abe7c1bff1620ab0eb90bcd
hxu1 <86326640+hxu1@users.noreply.github.com>
2024-07-01 18:52:55 +0000

    fix: polkit syntax

--- sudo-no-password.sh
+++ sudo-no-password.sh
@@ -7,10 +7,10 @@ set -o pipefail
 echo "${USER} ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/${USER}"
 
 POLKIT_FILE='/etc/polkit-1/rules.d/10-disable-password-prompt.rules'
-echo 'polkit.addRule((action, subject) => {' | sudo tee          "${POLKIT_FILE}"
-echo "  if (subject.user === \"${USER}\") {" | sudo tee --append "${POLKIT_FILE}"
-echo '    return polkit.Result.YES;'         | sudo tee --append "${POLKIT_FILE}"
-echo '  }'                                   | sudo tee --append "${POLKIT_FILE}"
-echo '});'                                   | sudo tee --append "${POLKIT_FILE}"
+echo 'polkit.addRule(function (action, subject) {' | sudo tee          "${POLKIT_FILE}"
+echo "  if (subject.user === \"${USER}\") {"       | sudo tee --append "${POLKIT_FILE}"
+echo '    return polkit.Result.YES;'               | sudo tee --append "${POLKIT_FILE}"
+echo '  }'                                         | sudo tee --append "${POLKIT_FILE}"
+echo '});'                                         | sudo tee --append "${POLKIT_FILE}"
 
 echo 'done'
