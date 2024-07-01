df98f32647ef1430c1f4f6e5ace436f40fac6c81
hxu1 <86326640+hxu1@users.noreply.github.com>
2024-07-01 18:48:38 +0000

    fix: use rules

--- sudo-no-password.sh
+++ sudo-no-password.sh
@@ -6,11 +6,11 @@ set -o pipefail
 
 echo "${USER} ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/${USER}"
 
-sudo mkdir --parents /var/lib/polkit-1/localauthority/50-local.d
-POLKIT_FILE=/var/lib/polkit-1/localauthority/50-local.d/10-disable-password-prompt.pkla
-echo '[Disable password prompt]'  | sudo tee          "${POLKIT_FILE}"
-echo "Identity=unix-user:${USER}" | sudo tee --append "${POLKIT_FILE}"
-echo 'Action=*'                   | sudo tee --append "${POLKIT_FILE}"
-echo 'ResultAny=yes'              | sudo tee --append "${POLKIT_FILE}"
+POLKIT_FILE='/etc/polkit-1/rules.d/10-disable-password-prompt.rules'
+echo 'polkit.addRule((action, subject) => {' | sudo tee          "${POLKIT_FILE}"
+echo "  if (subject.user === \"${USER}\") {" | sudo tee --append "${POLKIT_FILE}"
+echo '    return polkit.Result.YES;'         | sudo tee --append "${POLKIT_FILE}"
+echo '  }'                                   | sudo tee --append "${POLKIT_FILE}"
+echo '});'                                   | sudo tee --append "${POLKIT_FILE}"
 
 echo 'done'
